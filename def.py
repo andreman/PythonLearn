eafe06af4a7c66653883402d475c13ab68d1cda4
andrew <andrewevg@gmail.com>
2022-11-06 21:55:29 +0700

    первые задачи c функцией

---
+++ def.py
@@ -0,0 +1,15 @@
+def hello():
+    print('Hello, world')
+
+hello()
+
+x = int(input('Enter 1st number'))
+y = int(input('Enter 2nd number'))
+
+def sum(a, b):
+    p = a*4
+    s = a*a
+    print('p=', p, 's=', s)
+sum(x, y)
+
+
