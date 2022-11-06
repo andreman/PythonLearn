5c17a9087e31bc6345a8548cea5ed01ccc8c8ed1
andrew <andrewevg@gmail.com>
2022-11-06 19:34:51 +0700

    первые задачи

---
+++ kate_1.py
@@ -0,0 +1,13 @@
+a:str = 'Hello Kate_'
+print(a, a)
+
+age = int(input('How o;d are u?'))
+if (age >= 25):
+    print("Welcome")
+elif (age>=18) and (age < 25):
+    print('Only with Mom')
+else:
+    print("DO not come in")
+
+
+
---
+++ kate_2.py
@@ -0,0 +1,5 @@
+age: int
+print('How old are u?')
+input(age)
+if (age > = 18):
+    print('Good')
